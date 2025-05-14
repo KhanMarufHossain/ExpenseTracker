ef2d0ee3434b667b7c1c05ab72f296efb7b00074
Khan Md. Maruf Hossain <khan.hossain.242@northsouth.edu>
2025-05-14 14:42:41 +0600

    New thing

---
+++ Screens/Home.js
@@ -0,0 +1,15 @@
+import {View, Text,StyleSheet} from 'react-native';
+export default function Home ()
+{
+    return(
+        <View style={styles.maincontainer} >
+            <Text></Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    maincontainer: {
+        flex: 1,
+    }
+});
