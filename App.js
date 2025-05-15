f8d17d2e8cfe3bd59fde6e16b6dbba5f3535cbfe
Khan Md. Maruf Hossain <khan.hossain.242@northsouth.edu>
2025-05-15 22:22:45 +0600

    last commit

--- App.js
+++ App.js
@@ -1,40 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-import Home from './Screens/Home';
-import ChooseCurrency from './Screens/Currency';
-import { Store } from './Store/store.js';
-import { Provider, useSelector } from 'react-redux';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import Home from "./Screens/Home";
+import ChooseCurrency from "./Screens/Currency";
+import { Store } from "./Store/store.js";
+import { Provider, useSelector } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
-function DrawerNavigation(){
+function DrawerNavigation() {
   const currency = useSelector((store) => store.Currency);
   const initialRoute = currency?.code ? "Home" : "Currency";
-  return(
+  return (
     <Drawer.Navigator
-    initialRouteName= {initialRoute}>
-      <Drawer.Screen  name="Currency"
-        component={ChooseCurrency}
-        options={{
-          headerShown: true,
-          title: "Select Currency"
-        }} />
-
+      screenOptions={{}}
+      initialRouteName={initialRoute}
+      backBehavior="firstroute"
+    >
       <Drawer.Screen
-        name="Home" 
+        name="Home"
         component={Home}
         options={{
           title: "Expense Tracker",
           headerBackTitle: "Back",
         }}
       />
+      <Drawer.Screen
+        name="Currency"
+        component={ChooseCurrency}
+        options={{
+          headerShown: true,
+          title: "Select Currency",
+        }}
+      />
     </Drawer.Navigator>
   );
-};
-
+}
 
 export default function App() {
   return (
@@ -50,8 +53,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
