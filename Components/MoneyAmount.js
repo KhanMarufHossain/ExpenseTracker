880741d80bdbadf3f429cf45cb4a1d786b016267
Khan Md. Maruf Hossain <khan.hossain.242@northsouth.edu>
2025-05-16 17:05:53 +0600

    polishing the styling of MoneyAmount

--- Components/MoneyAmount.js
+++ Components/MoneyAmount.js
@@ -3,12 +3,22 @@ import { useSelector } from "react-redux";
 
 export default function MoneyAmount() {
   
-  const Currency= useSelector((store)=>store.Currency);
-  const Balance = Currency.income- Currency.expense;
+  const Currency = useSelector((store) => store.Currency);
+  const Balance = Currency.income - Currency.expense;
+  const isNegative = Balance < 0;
+  
   return (
-    <View style={styles.mainContainer}>
+    <View style={[
+      styles.mainContainer, 
+      isNegative ? styles.negativeContainer : null
+    ]}>
       <Text style={styles.currencycode}>{Currency.code}</Text>
-      <Text style={styles.moneyText}>{Balance}</Text>
+      <Text style={[
+        styles.moneyText, 
+        isNegative ? styles.negativeText : null
+      ]}>
+        {isNegative ? `-${Math.abs(Balance)}` : Balance}
+      </Text>
     </View>
   );
 }
@@ -25,6 +35,9 @@ const styles = StyleSheet.create({
     elevation: 4,
     flexDirection: "row",
   },
+  negativeContainer: {
+    backgroundColor: "#d32f2f",
+  },
   currencycode: {
     fontSize: 20,
     fontWeight: "bold",
@@ -37,5 +50,8 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#fff",
     letterSpacing: 1,
+  },
+  negativeText: {
+    color: "#ffcdd2",
   },
 });
