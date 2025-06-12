47fea6f4ba8908afd35c0565ebd21d8b51558e04
KhanMarufHossain <khan.hossain.242@northsouth.edu>
2025-06-12 20:24:17 +0600

    added authSlice to store.js

--- Store/store.js
+++ Store/store.js
@@ -1,10 +1,11 @@
 import CurrencyReducer from './CurrencySlice.js'
 import { configureStore } from '@reduxjs/toolkit'
 import { persistenceMiddleware } from './persistenceMiddleware.js'
-
+import authReducer from './authSlice.js';
 export const Store = configureStore({
     reducer: {
         Currency: CurrencyReducer,
+        auth : authReducer,
     },
     middleware: (getDefaultMiddleware) => 
         getDefaultMiddleware().concat(persistenceMiddleware)
