d7dbe91ae7d00f655bb6247a99ca9e01f11b5e25
KhanMarufHossain <khan.hossain.242@northsouth.edu>
2025-06-12 20:20:20 +0600

    authenticationSlice made

---
+++ Store/authSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+const authSlice = createSlice(
+    {
+        name: "auth",
+        initialState: {
+            user : null,
+            loading: false,
+            error: null,
+        
+        },
+
+        reducers: {
+            setUser (state, action){
+                state.user = action.payload;
+                state.error = null;
+            },
+            setLoading(state,action)
+            {
+                state.loading= action.payload;
+            },
+            setError (state, action)
+            {
+                state.error = action.payload;
+            },
+            logout (state){
+                state.user = null;
+                state.error = null;
+            }
+
+        }
+    }
+);
+
+export const {setUser, setLoading, setError, logout}= authSlice.actions;
+export default authSlice.reducer;
