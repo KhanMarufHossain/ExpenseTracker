9b39ee134f87706da63aed8fa93e8b1081c37191
KhanMarufHossain <khan.hossain.242@northsouth.edu>
2025-06-14 00:19:07 +0600

    making the userSpecific persmission settled

--- Store/transactionThunks.js
+++ Store/transactionThunks.js
@@ -1,5 +1,5 @@
 import { databases, account } from "./appwrite";
-import { Query } from "appwrite";
+import { Query, Permission, Role } from "appwrite";
 import { updateTransactionTrack, clearTransactions } from "./CurrencySlice";
 
 const DATABASE_ID = "684b0cf90019a37d16ee";
@@ -7,15 +7,20 @@ const COLLECTION_ID = "684bc7640025ef61ee6c";
 
 export const addTransaction = (transaction) => async (dispatch) => {
   try {
-    const user= await account.get();
-    const userId= user.$id;
+    const user = await account.get();
+    const userId = user.$id;
     const response = await databases.createDocument(
       DATABASE_ID,
       COLLECTION_ID,
       "unique()",
-      {...transaction, userId}
+      { ...transaction, userId },
+      [
+        Permission.read(Role.user(userId)),
+        Permission.update(Role.user(userId)),
+        Permission.delete(Role.user(userId)),
+        Permission.write(Role.user(userId)), // Optional, covers update/delete
+      ]
     );
-    
     dispatch(updateTransactionTrack(response));
   } catch (error) {
     console.error("Error adding transaction:", error);
@@ -24,16 +29,16 @@ export const addTransaction = (transaction) => async (dispatch) => {
 
 export const fetchTransactions = () => async (dispatch) => {
   try {
-    const user= await account.get();
+    const user = await account.get();
     const userId = user.$id;
 
     const response = await databases.listDocuments(
       DATABASE_ID,
       COLLECTION_ID,
-      [Query.equal("userId",userId)]
+      [Query.equal("userId", userId)]
     );
     dispatch(clearTransactions());
-    response.documents.forEach(tx => dispatch(updateTransactionTrack(tx)));
+    response.documents.forEach((tx) => dispatch(updateTransactionTrack(tx)));
   } catch (error) {
     console.error("Error fetching transactions:", error);
   }
